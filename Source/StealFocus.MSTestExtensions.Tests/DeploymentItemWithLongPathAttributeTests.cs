747396c060837c9af271927bfd91d3a6bba7cb2f
Rami Abughazaleh <rami.abughazaleh@gmail.com>
2013-04-05 13:21:03 -0700

    creating test files and directories in ClassInitialize and ClassCleanup instead of using static test files

--- Source/StealFocus.MSTestExtensions.Tests/DeploymentItemWithLongPathAttributeTests.cs
+++ Source/StealFocus.MSTestExtensions.Tests/DeploymentItemWithLongPathAttributeTests.cs
@@ -8,6 +8,7 @@
 // ---------------------------------------------------------------------------------------------------------------------
 namespace StealFocus.MSTestExtensions.Tests
 {
+    using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -19,20 +20,88 @@ namespace StealFocus.MSTestExtensions.Tests
         private const int MaximumPathLength = 248 - 1 /* null terminating character */;
         private const int MaximumFilenameLength = 260;
 
-        private const string ProjectPath = "StealFocus.MSTestExtensions.Tests\\";
+        private const char PathSeparator = @"\";
+        private const string ProjectPath = "StealFocus.MSTestExtensions.Tests";
 
         private const string ShortTestFile1DotTxt = "TestFile1.txt";
-        private const string ShortTestFile1DotTxtPath = ProjectPath + ShortTestFile1DotTxt;
+        private const string ShortTestFile1DotTxtPath = ProjectPath + PathSeparator + ShortTestFile1DotTxt;
         private const string ShortOutputDirectoryName = "ShortDirectoryName";
 
         private const string TestFileADotTxt = "TestFileA.txt";
 
-        private const string ShortDirectoryPath = ProjectPath + "TestData";
+        private const string ShortDirectoryPath = ProjectPath + PathSeparator + "TestData";
 
         private const string LongTestFile1DotTxt = "a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long file name 1.txt";
-        private const string LongTestFile1DotTxtPath = ProjectPath + LongTestFile1DotTxt;
-        private const string LongDirectoryPath = ProjectPath + "a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long directory name";
+        private const string LongTestFile1DotTxtPath = ProjectPath + PathSeparator + LongTestFile1DotTxt;
+        private const string LongDirectoryPath = ProjectPath + PathSeparator + "a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long directory name";
         private const string LongOutputDirectoryName = "a very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long output directory name";
+
+        private const string TestFileContents = "This file is used for testing";
+
+        private static List<string> filesToDelete = new List<string>();
+        private static List<string> directoriesToDelete = new List<string>();
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            string solutionPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+            // create TestFile1.txt
+            string shortTestFile1DotTxtFullPath = Path.Combine(solutionPath, ShortTestFile1DotTxtPath);
+            File.WriteAllText(shortTestFile1DotTxtFullPath, TestFileContents);
+
+            filesToDelete.Add(shortTestFile1DotTxtFullPath);
+
+            // create TestData
+            string shortDirectoryFullPath = Path.Combine(solutionPath, ShortDirectoryPath);
+            Directory.CreateDirectory(shortDirectoryFullPath);
+
+            directoriesToDelete.Add(shortDirectoryFullPath);
+
+            // create TestData\TestFileA.txt
+            string shortTestFileADotTxtFilePath = Path.Combine(shortDirectoryFullPath, TestFileADotTxt);
+            ZlpIOHelper.WriteAllText(shortTestFileADotTxtFilePath, TestFileContents);
+
+            filesToDelete.Add(shortTestFileADotTxtFilePath);
+
+            // create "a very ... long directory name"
+            string longDirectoryFullPath = Path.Combine(solutionPath, LongDirectoryPath);
+            ZetaLongPaths.ZlpIOHelper.CreateDirectory(longDirectoryFullPath);
+
+            directoriesToDelete.Add(longDirectoryFullPath);
+
+            // create "a very ... long directory name\TestFileA.txt"
+            string longTestFileADotTxtFilePath = Path.Combine(longDirectoryFullPath, TestFileADotTxt);
+            ZlpIOHelper.WriteAllText(longTestFileADotTxtFilePath, TestFileContents);
+
+            filesToDelete.Add(longTestFileADotTxtFilePath);
+
+            // create "a very ... long file name 1.txt"
+            string longTestFile1DotTxtFilePath = Path.Combine(solutionPath, LongTestFile1DotTxtPath);
+            ZlpIOHelper.WriteAllText(longTestFile1DotTxtFilePath, TestFileContents);
+
+            filesToDelete.Add(longTestFile1DotTxtFilePath);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            foreach (string path in directoriesToDelete)
+            {
+                if (ZetaLongPaths.ZlpIOHelper.DirectoryExists(path))
+                {
+                    ZetaLongPaths.ZlpIOHelper.DeleteDirectory(path, true);
+                }
+            }
+
+            foreach (string filePath in filesToDelete)
+            {
+                if (ZetaLongPaths.ZlpIOHelper.FileExists(filePath))
+                {
+                    ZetaLongPaths.ZlpIOHelper.DeleteFile(filePath);
+                }
+            }
+        }
 
         [TestMethod]
         [DeploymentItemWithLongPath(ShortTestFile1DotTxtPath)]
